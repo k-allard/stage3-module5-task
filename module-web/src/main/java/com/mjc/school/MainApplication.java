37b48e1409795a00ea85dbb58475bfc3bbc8bbd8
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-09-02 20:01:44 +0400

    Fixing versions

--- module-web/src/main/java/com/mjc/school/MainApplication.java
+++ module-web/src/main/java/com/mjc/school/MainApplication.java
@@ -2,10 +2,23 @@ package com.mjc.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class MainApplication {
+    private static ApplicationContext applicationContext;
+
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        applicationContext = SpringApplication.run(MainApplication.class, args);
+        displayAllBeans();
+    }
+
+    public static void displayAllBeans() {
+        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        for(String beanName : allBeanNames) {
+            System.out.println(beanName);
+        }
     }
 }
--- module-web/src/main/java/com/mjc/school/controller/config/MainWebAppInitializer.java
+++ module-web/src/main/java/com/mjc/school/controller/config/MainWebAppInitializer.java
@@ -1,25 +1,26 @@
 package com.mjc.school.controller.config;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
 public class MainWebAppInitializer implements WebApplicationInitializer {
     @Override
-    public void onStartup(final ServletContext sc) {
+    public void onStartup(final ServletContext servletContext) {
 
         AnnotationConfigWebApplicationContext root =
                 new AnnotationConfigWebApplicationContext();
 
         root.scan("com.mjc.school");
-        sc.addListener(new ContextLoaderListener(root));
+        servletContext.addListener(new ContextLoaderListener(root));
 
         ServletRegistration.Dynamic appServlet =
-                sc.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext()));
+                servletContext.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext()));
         appServlet.setLoadOnStartup(1);
         appServlet.addMapping("/");
     }
